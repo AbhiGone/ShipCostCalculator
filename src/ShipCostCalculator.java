06d97321f1cbdf935cd3b76ed15fe553e1ed1f5b
AbhiGone <giriabhinaya@gmail.com>
2023-09-27 09:12:14 -0400

    Shipping calculator

--- src/ShipCostCalculator.java
+++ src/ShipCostCalculator.java
@@ -1,5 +1,20 @@
-public class Main {
+import java.util.Scanner;
+
+public class ShipCostCalculator {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        double ship = .02;
+        double shippingCost = 0;
+        double limit = 100;
+        double total;
+
+        System.out.println("Enter the price of your item");
+        double item = scan.nextDouble();
+
+        if(item >= limit){
+            shippingCost = ship * item;
+        }
+        total = item + shippingCost;
+        System.out.println("Your shipping cost is, $" + shippingCost + " your total is, $" + total);
     }
 }
